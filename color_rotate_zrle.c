dcf9e16b2516c04a505283d3c1c226d13eb4c348
David Dalrymple (on zayin) <davidad@alum.mit.edu>
2014-03-28 16:20:40 -0400

    open_port

--- color_rotate_zrle.c
+++ color_rotate_zrle.c
@@ -10,7 +10,17 @@
 #define FBUFZ 1000
 
 #include "colorspaces.c"
-#include "open_port.c"
+
+int open_port(uint16_t port) {
+  int connfd, sockfd, y[1]={1};
+  struct sockaddr_in addr = {.sin_family=AF_INET,.sin_port=htons(port),.sin_addr={.s_addr=htonl(INADDR_ANY)}};
+  if( ( sockfd = socket(PF_INET, SOCK_STREAM, 0)                         ) < 0)  perror(  "socket"  );
+  if( (      setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, y, sizeof(int))) < 0)  perror("setsockopt");
+  if( (            bind(sockfd, (struct sockaddr*)&addr, sizeof(addr))   ) < 0)  perror(   "bind"   );
+  if( (          listen(sockfd, 1)                                       ) < 0)  perror(  "listen"  );
+  if( ( connfd = accept(sockfd, NULL, 0)                                 ) < 0)  perror(  "accept"  );
+  return connfd;
+}
 
 const char protover[] = "RFB 003.003\n";
 const char securitytype[] = {0x00, 0x00, 0x00, 0x02};
